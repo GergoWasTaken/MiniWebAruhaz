ae1d3cad983e08cd554de63d8bc272e43b564428
unknown <Csider Levente@NET1.LOCAL>
2026-04-14 08:03:13 +0200

    Post függvény váza és a becsatolandó id-k hozzáadása

--- Js/post.js
+++ Js/post.js
@@ -1,1 +1,21 @@
-document.getElementById("urlap").addEventListener("submit ")
+document.getElementById("urlap").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const nev = document.getElementById("nev").value
+    const ar = document.getElementById("ar").value
+    const leiras = document.getElementById("leiras").value
+    const kep = document.getElementById("kep").value
+
+    const adat = {
+        "nev": nev,
+        "ar": ar,
+        "leiras": leiras,
+        "kep": kep
+    };
+
+    const csomag = {
+        "method": PUT,
+        headers: {
+            "Content-Type": "application/js"
+        }
+    }
+})
